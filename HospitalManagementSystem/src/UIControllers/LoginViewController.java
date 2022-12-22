d455e03490cfe09edf033f54eede406a6aa18348
xYoussefMahmoudx <youssef.mahmoud.eng@gmail.com>
2022-12-22 19:28:37 +0200

    login cont

--- HospitalManagementSystem/src/UIControllers/LoginViewController.java
+++ HospitalManagementSystem/src/UIControllers/LoginViewController.java
@@ -1,5 +1,19 @@
 package UIControllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class LoginViewController {
 
+    @FXML
+    void doctorLoginFunc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void secretaryLoginFunc(ActionEvent event) {
+
+    }
+
 }
+
