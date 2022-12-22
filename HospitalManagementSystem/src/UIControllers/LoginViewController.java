3c36bb4c0e402af5d5cdc637614747970ff3c6a6
M0hAyman <m7amadaymankh@gmail.com>
2022-12-22 23:27:06 +0200

    LoginViewController, initiateExcuseController and displayScheduleController functions implemented. Added some ids in fxml files and added prompted text for the combo-boxes

--- HospitalManagementSystem/src/UIControllers/LoginViewController.java
+++ HospitalManagementSystem/src/UIControllers/LoginViewController.java
@@ -1,17 +1,57 @@
 package UIControllers;
 
+import UIFiles.*;
+import application.SystemManager;
+
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class LoginViewController {
+    SystemManager sys = SystemManager.Singleton();
+    @FXML
+    private Label statusLoginlbl;
+    @FXML
+    private TextField userNameTxt;
+    @FXML
+    private TextField passwordTxt;
+
+    String userN;
+    String pass;
 
     @FXML
     void doctorLoginFunc(ActionEvent event) {
+
+        userN = userNameTxt.getText();
+        pass = passwordTxt.getText();
+        if(userN==null || pass ==null)
+        {
+            statusLoginlbl.setText("Error! Unsuccessful login! User Name or Password is missing");
+        }
+        else
+        {
+            statusLoginlbl.setText("Login Successfull");
+            sys.doctorLogin(userN,pass);
+        }
+
+
 
     }
 
     @FXML
     void secretaryLoginFunc(ActionEvent event) {
+
+        userN = userNameTxt.getText();
+        pass = passwordTxt.getText();
+        if(userN==null || pass ==null)
+        {
+            statusLoginlbl.setText("Error! Unsuccessful login! User Name or Password is missing");
+        }
+        else
+        {
+            statusLoginlbl.setText("Login Successfull");
+            sys.secretaryLogin(userN,pass);
+        }
 
     }
 
